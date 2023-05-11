377b7e40b6bcb30e5e52c0b0111236ea12765feb
arlanbutarbutar <arlan270899@gmail.com>
2023-05-11 21:28:04 +0800

    update: tentang

--- tentang.php
+++ tentang.php
@@ -18,7 +18,7 @@
   <section class="about_section layout_margin">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-md-5 offset-md-1">
+        <div class="col-lg-5 offset-md-1">
           <div class="detail-box">
             <div class="heading_container">
               <h2>
@@ -28,14 +28,162 @@
               </h2>
               <hr>
             </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae molestias placeat assumenda fugit, ut at alias dolor sapiente similique! Accusamus ea nihil non dolore tempora perspiciatis eum inventore. Modi voluptas iste vero voluptatem ducimus doloribus sequi error? Dolorum quia nesciunt incidunt dicta voluptate maiores molestias repellat assumenda, repellendus mollitia perspiciatis.
-            </p>
+            <h4>Sekilas Program Studi Ilmu Komputer</h4>
+            <p>Program Studi Ilmu Komputer adalah program studi dengan perkembangan globalisasi yang sangat pesat menuntut adanya pengetahuan dan penguasaan teknologi informasi. Kemampuan dan ketrampilan mengolah data informasi komputer menjadi modal utama bagi berbagai industri bisnis didunia global.</p>
+            <p>Kehadiran jurusan teknik informatika bertujuan menyiapkan mahasiswa agar mampu menguasai berbagai aspek yang berkaitan dengan tenologi sistem informasi komputer, baik perangkat lunak maupun perangkat keras (sistem digital), serta mampu dalam aplikasi, komunikasi dan pengadilan sistem komputer. Dengan didukung oleh tenaga dosen dan sarana laboratorium yang memadai; jurusan/program teknik informatika siap menghasilkan serjana yang mampu bersaing dalam dunia kerja.</p>
+            <p>Jurusan ini menerima lulusan Sekolah Menengah Atas (SMA) jurusan Ilmu Pengetahuan Alam (IPA), Sekolah Menengah Kejuruan (SMK) jurusan multi media, elektro dan jurusan lain yang sejenis. Lulusan dari Jurusan ini telah terserap di berbagai instansi pemerintah, Lembaga Swadaya Masyarakat (LSM), Badan Usaha Milik Negara (BUMN), dan lembaga swasta lainnya.</p>
           </div>
         </div>
-        <div class="col-md-6 px-0">
-          <div class="img-box">
+        <div class="col-lg-6">
+          <div class="img-box text-center mb-3">
             <img src="assets/images/about-img.jpg" style="width: 80%;border-radius: 20px;" alt="">
+          </div>
+        </div>
+        <div class="col-md-10 offset-md-1">
+          <div class="detail-box">
+            <h4>Sejarah Program Studi Ilmu Komputer</h4>
+            <p>Pada awal pendiriannya, Universitas Katolik Widya Mandira (UNWIRA) hanya terdiri dari 3 fakultas, yaitu Fakultas Keguruan dan Ilmu Pendidikan dan Fakultas Teknik yang berkedudukan di kupang serta Fakultas Filsafat dan Teologi Katolik yang berkedudukan di Ledalero Maumere-Flores, Fakultas Keguruan dan Ilmu Pendidikan (FKIP), dan Fakultas Teknik yang berkedudukan di Kupang. Namun pada tahun akademik 2000/2001 Universitas Katolik Widya Mandira Kupang kembali membuka lima (5) Program Studi Baru Jenjang Strata-1 (S1) salah satu diantaranya adalah Program Studi Ilmu Komputer yang berada pada Fakultas Teknik. Ilmu Komputer yang merupakan salah satu disiplin ilmu pada bidang Teknologi Informasi, dikembangkan untuk memberikan pengetahuan tentang manfaat Teknologi Informasi tersebut guna memenuhi kebutuhan-kebutuhan manusai yang semakin beragam dan kopleks. Program Studi Ilmu Komputer Universitas Katolik Widya Mandira Kupang, di arahkan untuk menghasilkan seorang analis dan programmer yang berlandaskan pada pengetahuan informatika teoretik dengan memaksimalkan manfaat teknologi yang diharapkan mempunyai kemampuan untuk memecahkan berbagai masalah dalam dunia nyata.</p>
+            <p>Didukung oleh tenaga dosen dengan rata-rata Strata-2 (S2) dan Strata-3 (S3) dengan kualifikasi keahlian yang merata dibidang multimedia seperti Data Mining, Multimedia, Geography Information Sistem (GIS), Rekayasa Perangkat Lunak, Rekayasa Perangkat Keras dan Artificial Intelligent, menjadikan program studi informatika mampu menghasilkan lulusan yang unggul dan siap memasuki dunia kerja. Dilengkapi dengan laboratorium dan perpustakaan program studi yang membantu mahasiswa dalam menunjang keahlian dari mahasiswa itu sendiri.</p>
+            <h4>Visi, Misi, dan Tujuan Program Studi Ilmu Komputer</h4>
+            <ol type="a">
+              <li>
+                <h6>Visi</h6>
+                <p>Menjadi unggul, berkarakter, dan pelopor dalam bidang informatika yang berfokus pada pengembangan mobile dan soft computing berwawasan global dan berakar pada budaya lokal berdasarkan nilai-nilai kristiani</p>
+              </li>
+              <li>
+                <h6>Misi</h6>
+                <ul>
+                  <li>
+                    <p>Menyelenggarakan pembelajaran yang berkualitas dan berkarakter untuk menghasilkan lulusan yang siap pakai dengan keahlian di bidang mobile dan soft computing</p>
+                  </li>
+                  <li>
+                    <p>Melaksanakan kegiatan penelitian di bidang mobile dan soft computing yang berkualitas dan bermanfaat bagi masyarakat</p>
+                  </li>
+                  <li>
+                    <p>Melaksanakan pengabdian masyarakat yang berkualitas dalam bidang mobile dan soft computing dalam rangka menjawab kebutuhan masyarakat</p>
+                  </li>
+                  <li>
+                    <p>Melaksanakan sistem penjaminan mutu internal Program Studi Teknik Informatika untuk menghasilkan lulusan dan mutu tata kelola dalam penyelenggaraan administrasi yang terukur dan berkelanjutan</p>
+                  </li>
+                  <li>
+                    <p>Menghasilkan lulusan yang kreatif dan inovatif menguasai bidang mobile dan soft computing, berwawasan global dan berakar pada budaya lokal serta berdasarkan nilai-nilai kristiani.</p>
+                  </li>
+                </ul>
+              </li>
+            </ol>
+            <h4>Struktur Organisasi Program Studi Ilmu Komputer</h4>
+            <div class="col text-center">
+              <img src="assets/images/struktur-organisasi-ilkom.png" alt="">
+            </div>
+            <p>Personil beserta fungsi dan tugas pokok:</p>
+            <ol type="1">
+              <li>
+                <p>Ketua Program Studi Ilmu Komputer</p>
+                <ul>
+                  <li>
+                    <p>Melakukan koordinasi seluruh penyelenggaraan pembelajaran di program studi.</p>
+                  </li>
+                  <li>
+                    <p>Mengkoordinir penelitian dan pengembangan ilmu pengetahuan, teknologi, dan seni serta perancanaan dan pelaksanaannya pengabdian kepada masyarakat.</p>
+                  </li>
+                  <li>
+                    <p>Mengkoordinir kegiatan ketatausahaan dan pelayanaan administrasi yang berkaitan dengan pembelajaran Sistem Informasi Akademik (SIMAK), pengurusan data mahasiswa, data hasil ujian, Form Rencana Studi (FRS), data Indeks Prestasi Semester /Indeks Prestasi Komulatif (IPS/IPK) di program studi.</p>
+                  </li>
+                  <li>
+                    <p>Melaksanakan pembinaan civitas akademik program studi ilmu komputer.</p>
+                  </li>
+                  <li>
+                    <p>Mengikuti seluruh aktivitas fakultas dan universitas.</p>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <p>Sekertaris Program Studi</p>
+                <p>Tugas pokoknya:</p>
+                <ul>
+                  <li>
+                    <p>Membantu ketua program studi dalam mengkoordinir penyelenggaraan Proses Belajar Mengajar (PBM).</p>
+                  </li>
+                  <li>
+                    <p>Membantu ketua program studi dalam kegiatan ketatausahan dan pelayananan administrasi.</p>
+                  </li>
+                  <li>
+                    <p>Bersama ketua program studi mengkoordinir kegiatan kemahasiswaan</p>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <p>Tata Usaha Program Studi</p>
+                <p>Tugas pokoknya:</p>
+                <ul>
+                  <li>
+                    <p>Melaksananakan kegiatan ketatausahaan dan pelayanaan administrasi program studi.</p>
+                  </li>
+                  <li>
+                    <p>Melayani kebutuhan mahasiswa terkait surat keterangan untuk keperluan studi dan tugas akhir.</p>
+                  </li>
+                  <li>
+                    <p>Mengatur dan menyimpan arsip surat /dokumen, data mahasiswa, hasil ujian, Kartu Rencana Studi (KRS), membuat fom presentasi kuliah.</p>
+                  </li>
+                  <li>
+                    <p>Dalam himpunan mahasiswa program studi ilmu komputer.</p>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <p>Kepala Laboratorium Digital Ilmu Komputer</p>
+                <p>Tugas pokoknya:</p>
+                <ul>
+                  <li>
+                    <p>Membantu ketua program studi, dalam penyelenggaraan laboratorium komputer yang meliputi: mengatur penjadwalan, penggunaan laboratorium, kerja praktik laboratorium, aktivitas perkuliahan Computer Aided Design (CAD).</p>
+                  </li>
+                  <li>
+                    <p>Menjaga seluruh kelayakan peralatan yang ada dan mengatur peralatan komputer.</p>
+                  </li>
+                  <li>
+                    <p>Mengembangkan kemampuan laboratorium yang ada sehingga dapat memberikan nilai tambah kepada Program Studi /Fakultas Teknik / Universitas.</p>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <p>Kelompok Dosen</p>
+                <p>Menyelenggaraan kegiatan pembelajaran yang berkaitan dengan keahlian dosen dan rumpun mata kuliah.</p>
+              </li>
+              <li>
+                <p>Himpunan Mahasiswa Program Studi (HMPS) Program Studi</p>
+                <ul>
+                  <li>
+                    <p>Menentukan visi dan misi HMPS</p>
+                  </li>
+                  <li>
+                    <p>Membuat program kerja himpunan mahasiswa program studi</p>
+                  </li>
+                  <li>
+                    <p>Melaksanakan kegiatan himpunan mahasiswa program studi</p>
+                  </li>
+                  <li>
+                    <p>Meminta persetujuan kegiatan kepada ketua program studi</p>
+                  </li>
+                  <li>
+                    <p>Membuat laporan pertanggung jawaban.</p>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <p>Mahasiswa</p>
+                <ul>
+                  <li>
+                    <p>Melaksanakan kegiatan pendidikan</p>
+                  </li>
+                  <li>
+                    <p>Melakukan penelitian</p>
+                  </li>
+                  <li>
+                    <p>Pengabdian kepada masyarakat</p>
+                  </li>
+                </ul>
+              </li>
+            </ol>
           </div>
         </div>
       </div>
